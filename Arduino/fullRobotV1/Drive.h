e3a5c384504e7d20e49483e01bc7e66facd10196
Jeffrey Fisher II <jeffreyspaceboy@gmail.com>
2020-02-15 23:24:30 -0600

    Adding turning functions

--- Arduino/fullRobotV1/Drive.h
+++ Arduino/fullRobotV1/Drive.h
@@ -1,5 +1,6 @@
 #include <AFMotor.h>
 
+
 AF_DCMotor motorLF(1);  //motor setup
 AF_DCMotor motorLB(2);
 AF_DCMotor motorRB(3);
@@ -7,12 +8,38 @@ AF_DCMotor motorRF(4);
 
 class Drive{
   private:
-  
+    
   public:
     void motorStop();
     void motorForward();
     void motorBackward();
 };
+
+
+void towardsWall(int leftSpeed, int rightSpeed){
+  motorForward();
+  motorLF.setSpeed(highSpeed-30);
+  motorLB.setSpeed(highSpeed-30);
+  motorRF.setSpeed(lowSpeed);
+  motorRB.setSpeed(lowSpeed);
+}
+
+void awayWall(int leftSpeed, int rightSpeed){
+  motorForward();
+  motorLF.setSpeed(lowSpeed);
+  motorLB.setSpeed(lowSpeed);
+  motorRF.setSpeed(highSpeed+30);
+  motorRB.setSpeed(highSpeed+30);
+}
+
+void straight(){
+  motorForward();
+  motorRF.setSpeed(highSpeed);
+  motorRB.setSpeed(highSpeed);
+  motorLF.setSpeed(highSpeed-40);
+  motorLB.setSpeed(highSpeed-40);
+}
+
 
 void Drive::motorStop(){
   motorLF.run(RELEASE);
